e6b32ed450c3562eb9ded99b74d3014ceca56bbe
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2020-09-25 18:58:07 +0200

    Comment POST endpoint created

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommentController extends AbstractController
@@ -15,5 +17,17 @@ class CommentController extends AbstractController
     public function list_comments(CommentRepository $commentRepository)
     {
         return $this->json($commentRepository->findAll());
+    }
+
+    /**
+     * @Route("/game/{gameId}/comment", methods={"POST"})
+     * @param CommentRepository $commentRepository
+     */
+    public function add_comment(CommentRepository $commentRepository, Request $request, int $gameId): JsonResponse
+    {
+        $content = $request->getContent();
+        $data = json_decode($content);
+
+        return $this->json($data);
     }
 }
